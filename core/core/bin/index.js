6d2a99c74de66ffc845b9d58213e22ef1ad4a2d2
artist <306048913@qq.com>
2021-03-31 08:15:27 +0800

    初始化脚手架

---
+++ core/core/bin/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const utils = require('artist-caibo-utils')
+
+// module.exports = core;
+
+// function core () {
+// TODO
+// }
+
+console.log("core")
+console.log('utils', utils())
