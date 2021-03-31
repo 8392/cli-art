f93a16b636af7c5464bdc7024e34c83686cc6e25
artist <306048913@qq.com>
2021-03-31 08:16:11 +0800

    初始化脚手架目录结构

--- packages/core/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// module.exports = core;
-
-// function core () {
-// TODO
-// }
-
-console.log("core")
---
+++ utils/utils/bin/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = utils;
+
+function utils () {
+  return {
+    name: '张三',
+    age: 22,
+    city: '成都'
+  }
+}
